c6638f360921d08e5873e0182938c846031e4092
Rifat Khan <rifat6633@gmail.com>
2025-05-11 22:47:47 +0600

    chore(web): add header.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import fonts from "@/lib/fonts";
-import "@workspace/ui/globals.css";
+import Logo from "@workspace/design-system/components/Logo";
+import "@workspace/design-system/globals.css";
+import { Icons } from "@workspace/design-system/icons";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -9,8 +12,22 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fonts.GeistSans.variable} ${fonts.GeistSans.variable} font-sans antialiased `}
+        className={`${fonts.GeistSans.variable} ${fonts.GeistSans.variable} font-sans antialiased`}
       >
+        <header className="border-b py-5">
+          <div className="mx-auto flex w-full max-w-5xl items-center justify-between px-5 md:px-0">
+            <Link href={"/"} className="inline-block">
+              <Logo />
+            </Link>
+
+            <div>
+              <Link href="https://github.com/StepAsideLiL/print-med-prescription">
+                <Icons.Github size={40} />
+              </Link>
+            </div>
+          </div>
+        </header>
+
         {children}
       </body>
     </html>
