6a2b1c49b5da1dd464d5d81769e8053dfb0220a0
Rifat Khan <rifat6633@gmail.com>
2025-05-11 22:16:32 +0600

    feat(ui): add Logo components.

---
+++ packages/design-system/src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="border-foreground flex flex-col border px-1 py-3">
+      <span className="leading-1">Print</span>
+      <span className="text-4xl font-black">Med.</span>
+      <span className="leading-1">Prescription</span>
+    </div>
+  );
+}
