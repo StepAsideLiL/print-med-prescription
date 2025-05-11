c61c90e7950f7614fef6be31271044bd14abf10f
Rifat Khan <rifat6633@gmail.com>
2025-05-11 22:25:21 +0600

    chore(ui): apply small font.

--- packages/design-system/src/components/Logo.tsx
+++ packages/design-system/src/components/Logo.tsx
@@ -1,9 +1,9 @@
 export default function Logo() {
   return (
     <div className="border-foreground flex flex-col border px-1 py-3">
-      <span className="leading-1">Print</span>
-      <span className="text-4xl font-black">Med.</span>
-      <span className="leading-1">Prescription</span>
+      <span className="text-xs leading-1">Print</span>
+      <span className="text-3xl font-black">Med</span>
+      <span className="text-xs leading-1">Prescription</span>
     </div>
   );
 }
