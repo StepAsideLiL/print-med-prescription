1fed6bc24b2a8f270957ff2a37471653d1b90209
Rifat Khan <rifat6633@gmail.com>
2025-05-14 07:58:37 +0600

    chore(web): complete medicine list table.

--- apps/web/src/components/MedicineList.tsx
+++ apps/web/src/components/MedicineList.tsx
@@ -9,34 +9,55 @@ import {
   TableHeader,
   TableRow,
 } from "@workspace/design-system/ui/table";
+import { Fragment } from "react";
 
 export default function MedicineList() {
   const { get: medList } = store.MedList();
 
   return (
-    <Table>
+    <Table className="text-xs">
       <TableHeader>
         <TableRow>
+          <TableHead className="w-20 border text-center">Med Type</TableHead>
           <TableHead className=""></TableHead>
           <TableHead className="w-20 border text-center">Morning</TableHead>
           <TableHead className="w-20 border text-center">Noon</TableHead>
           <TableHead className="w-20 border text-center">Night</TableHead>
           <TableHead className="w-20 border text-center">Before Meal</TableHead>
           <TableHead className="w-20 border text-center">After Meal</TableHead>
+          <TableHead className="w-20 border text-center">Duration</TableHead>
         </TableRow>
       </TableHeader>
 
       {medList.length === 0 ? null : (
         <TableBody>
           {medList.map((med) => (
-            <TableRow key={med.id}>
-              <TableCell></TableCell>
-              <TableCell>{med.moring ? "1" : "0"}</TableCell>
-              <TableCell>{med.noon ? "1" : "0"}</TableCell>
-              <TableCell>{med.night ? "1" : "0"}</TableCell>
-              <TableCell>{med.afterMeal ? "Yes" : "No"}</TableCell>
-              <TableCell>{med.afterMeal ? "No" : "Yes"}</TableCell>
-            </TableRow>
+            <Fragment key={med.id}>
+              <TableRow key={med.id}>
+                <TableCell>
+                  {med.type === "tablet" ? "Tablet" : "Syrup"}
+                </TableCell>
+                <TableCell></TableCell>
+                <TableCell className="text-center">
+                  {med.moring ? "1" : "0"}
+                </TableCell>
+                <TableCell className="text-center">
+                  {med.noon ? "1" : "0"}
+                </TableCell>
+                <TableCell className="text-center">
+                  {med.night ? "1" : "0"}
+                </TableCell>
+                <TableCell className="text-center">
+                  {med.afterMeal ? "Yes" : "No"}
+                </TableCell>
+                <TableCell className="text-center">
+                  {med.afterMeal ? "No" : "Yes"}
+                </TableCell>
+                <TableCell className="text-center">
+                  {med.duration.lenght} {med.duration.unit}
+                </TableCell>
+              </TableRow>
+            </Fragment>
           ))}
         </TableBody>
       )}
