0393fba7b2b5ac556450e79f9227a8c4425373f3
Rifat Khan <rifat6633@gmail.com>
2025-05-18 23:09:56 +0600

    chore(web): update metadata.

--- apps/web/src/app/(edit)/create-template/page.tsx
+++ apps/web/src/app/(edit)/create-template/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Create Template",
+  title: "Create Prescription Template",
 };
 
 export default function Page() {
