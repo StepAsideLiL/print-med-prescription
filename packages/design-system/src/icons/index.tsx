7800229e1da08e2467ca54a984e580fb829bf2f4
Rifat Khan <rifat6633@gmail.com>
2025-05-21 17:06:06 +0600

    chore(design-system): add Bold, Italic, Heading, Paragraph and Underline icons.

--- packages/design-system/src/icons/index.tsx
+++ packages/design-system/src/icons/index.tsx
@@ -1,5 +1,10 @@
 import * as R from "@remixicon/react";
 
 export default {
+  Bold: R.RiBold,
+  Italic: R.RiItalic,
+  Heading: R.RiHeading,
   Github: R.RiGithubFill,
+  Paragraph: R.RiParagraph,
+  Underline: R.RiUnderline,
 } as const;
