acace4f64b0a0619660c717a1f5b700abd61ad4d
Rifat Khan <rifat6633@gmail.com>
2025-05-21 09:15:06 +0600

    feat(web): implement save functionality and save.

--- apps/web/src/components/editor/ImageEditor.tsx
+++ apps/web/src/components/editor/ImageEditor.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { store } from "@/lib/store";
 import { THeaderSection } from "@/lib/types";
+import { Button } from "@workspace/design-system/ui/button";
 import {
   Document,
   EditorContent,
@@ -8,21 +10,34 @@ import {
   Text,
   useEditor,
 } from "@workspace/editor";
+import React from "react";
 
 export default function ImageEditor({ section }: { section: THeaderSection }) {
+  const { updateHeaderImage } = store.UpdateHeaderImage();
+
   const editor = useEditor({
     extensions: [Document, Text, Image],
     content: section.content,
-    onUpdate: ({ editor }) => {
-      console.log(editor.getJSON());
-    },
-    onFocus: ({ editor }) => {
-      console.log(editor.getJSON());
-    },
   });
 
+  if (editor === null) {
+    return null;
+  }
+
   return (
-    <div style={section.style}>
+    <div style={section.style} className="relative">
+      <div className="bg-mute absolute -top-10 w-full rounded border p-1">
+        <Button
+          variant={"secondary"}
+          size={"sm"}
+          className="cursor-pointer"
+          onClick={() => {
+            updateHeaderImage({ ...section, content: editor.getJSON() });
+          }}
+        >
+          Save
+        </Button>
+      </div>
       <EditorContent editor={editor} />
     </div>
   );
--- apps/web/src/components/editor/TextEditor.tsx
+++ apps/web/src/components/editor/TextEditor.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { store } from "@/lib/store";
 import { THeaderSection } from "@/lib/types";
+import { Button } from "@workspace/design-system/ui/button";
 import {
   Document,
   EditorContent,
@@ -9,18 +11,34 @@ import {
   Text,
   useEditor,
 } from "@workspace/editor";
+import React from "react";
 
 export default function TextEditor({ section }: { section: THeaderSection }) {
+  const { updateHeaderText } = store.UpdateHeaderText();
+
   const editor = useEditor({
     extensions: [Document, Text, Paragraph, Heading],
     content: section.content,
-    onUpdate: ({ editor }) => {
-      console.log(editor.getJSON());
-    },
   });
 
+  if (editor === null) {
+    return null;
+  }
+
   return (
-    <div style={section.style}>
+    <div style={section.style} className="relative">
+      <div className="bg-muted absolute -top-14 w-full rounded border p-1">
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          className="cursor-pointer"
+          onClick={() => {
+            updateHeaderText({ ...section, content: editor.getJSON() });
+          }}
+        >
+          Save
+        </Button>
+      </div>
       <EditorContent editor={editor} />
     </div>
   );
