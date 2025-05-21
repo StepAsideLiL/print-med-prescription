90878fe089099a87027fd4aa4858d6ab78e91bb5
Rifat Khan <rifat6633@gmail.com>
2025-05-21 18:15:24 +0600

    chore(design-system): add delete icon.

--- packages/design-system/src/icons/index.tsx
+++ packages/design-system/src/icons/index.tsx
@@ -2,6 +2,7 @@ import * as R from "@remixicon/react";
 
 export default {
   Bold: R.RiBold,
+  Delete: R.RiDeleteBin2Line,
   Italic: R.RiItalic,
   Heading: R.RiHeading,
   Github: R.RiGithubFill,
