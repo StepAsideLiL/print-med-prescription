cb31af782a6223f68b888e530a54c0b751c71275
Rifat Khan <rifat6633@gmail.com>
2025-05-24 22:10:38 +0600

    feat(web): create save button to add template data in indexedDB.

--- apps/web/src/components/pdf-template/PrescriptionTemplate.tsx
+++ apps/web/src/components/pdf-template/PrescriptionTemplate.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import FooterSection from "./FooterSection";
+import SaveTemplateButton from "./SaveTemplateButton";
 import TemplateHeaderSection from "./TemplateHeaderSection";
 
 export default function PrescriptionTemplate() {
   return (
-    <section className="mx-auto py-5">
+    <section className="mx-auto space-y-2 py-5">
+      <div className="mx-auto w-full max-w-5xl">
+        <SaveTemplateButton />
+      </div>
+
       <div className="bg-background h-[1360px] w-[1000px]">
         <div className="flex h-full flex-col">
           <div className="flex-none px-10 pb-5 pt-10">
