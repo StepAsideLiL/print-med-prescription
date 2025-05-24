659015945e55f543279bd143780c7b2c90bc6aaa
Rifat Khan <rifat6633@gmail.com>
2025-05-24 20:21:50 +0600

    refactor(web): remove web worker related codes.

--- apps/web/src/components/ViewPdf.tsx
+++ apps/web/src/components/ViewPdf.tsx
@@ -3,33 +3,9 @@
 import { PDFViewer } from "@react-pdf/renderer";
 import PdfDoc from "./PdfDoc";
 import { store } from "@/lib/store";
-import { useEffect, useRef } from "react";
 
 export default function ViewPdf() {
   const { get } = store.MedicineList();
-  const workerRef = useRef<Worker>(null);
-
-  useEffect(() => {
-    console.log("Hello from useEffect!");
-
-    workerRef.current = new window.Worker(
-      /* webpackIgnore: true */ "/workers/db.worker.js",
-      {
-        type: "module",
-      }
-    );
-
-    workerRef.current.postMessage("Hello from nextjs client component");
-    workerRef.current.onmessage = (e) => {
-      console.log(`${e.data} to nextjs client component`);
-    };
-
-    return () => {
-      if (workerRef.current) {
-        workerRef.current.terminate();
-      }
-    };
-  }, []);
 
   if (get.length === 0) {
     return null;
