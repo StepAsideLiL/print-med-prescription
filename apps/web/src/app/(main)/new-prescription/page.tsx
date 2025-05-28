15fbb56dd28254b1442383d9305270839b90d2ef
Rifat Khan <rifat6633@gmail.com>
2025-05-28 17:42:45 +0600

    refactor(web): put content from home page to new-prescription page.

--- apps/web/src/app/(main)/new-prescription/page.tsx
+++ apps/web/src/app/(main)/new-prescription/page.tsx
@@ -1,9 +1,32 @@
+import AddMedicine from "@/components/AddMedicine";
+import MedicineList from "@/components/MedicineList";
+import PagePreview from "@/components/PagePreview";
+import SelectPageSize from "@/components/SelectPageSize";
+import { Button } from "@workspace/design-system/ui/button";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "New Prescription",
 };
 
 export default function Page() {
-  return <main>New Prescription</main>;
+  return (
+    <main className="mx-auto w-full max-w-5xl space-y-2 px-5 py-5 md:px-0">
+      <div className="flex items-center justify-between">
+        <SelectPageSize />
+
+        <Button variant={"outline"} asChild>
+          <Link href={"/preview"} target="_blank">
+            Preview
+          </Link>
+        </Button>
+      </div>
+
+      <PagePreview>
+        <MedicineList />
+        <AddMedicine />
+      </PagePreview>
+    </main>
+  );
 }
--- apps/web/src/app/(main)/page.tsx
+++ apps/web/src/app/(main)/page.tsx
@@ -1,10 +1,4 @@
-import AddMedicine from "@/components/AddMedicine";
-import MedicineList from "@/components/MedicineList";
-import PagePreview from "@/components/PagePreview";
-import SelectPageSize from "@/components/SelectPageSize";
-import { Button } from "@workspace/design-system/ui/button";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -13,18 +7,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="mx-auto w-full max-w-5xl space-y-2 px-5 py-5 md:px-0">
-      <div className="flex items-center justify-between">
-        <SelectPageSize />
-
-        <Button variant={"outline"} asChild>
-          <Link href={"/preview"}>Preview</Link>
-        </Button>
-      </div>
-
-      <PagePreview>
-        <MedicineList />
-        <AddMedicine />
-      </PagePreview>
+      hello
     </main>
   );
 }
