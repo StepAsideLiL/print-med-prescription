b32569377dd1049d14697d4ec71d73948dca9bc4
Rifat Khan <rifat6633@gmail.com>
2025-05-29 10:41:40 +0600

    feat(web): create all the necessary component.

--- apps/web/src/components/preview-pdf/PdfDoc.tsx
+++ apps/web/src/components/preview-pdf/PdfDoc.tsx
@@ -1,7 +1,20 @@
 "use client";
 
-import { TMedListSchema, TTemplate } from "@/lib/types";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import {
+  TImageContent,
+  TMedListSchema,
+  TTemplate,
+  TTemplateSectionSchema,
+} from "@/lib/types";
+import { JSONContent } from "@workspace/editor";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Image,
+} from "@react-pdf/renderer";
 
 const styles = StyleSheet.create({
   page: {
@@ -25,11 +38,13 @@ export default function PdfDoc({
   medList: TMedListSchema[];
   template: TTemplate;
 }) {
-  console.log(template);
-
   return (
     <Document pageLayout="twoColumnLeft">
       <Page size="A4" style={styles.page}>
+        {template.template.header.length !== 0 && (
+          <Header headerTemplate={template.template.header} />
+        )}
+
         <View style={styles.section}>
           {medList.map((med) => (
             <View key={med.id}>
@@ -37,7 +52,127 @@ export default function PdfDoc({
             </View>
           ))}
         </View>
+
+        {template.template.footer.length !== 0 && (
+          <Footer footerTemplate={template.template.footer} />
+        )}
       </Page>
     </Document>
+  );
+}
+
+function Header({
+  headerTemplate,
+}: {
+  headerTemplate: TTemplateSectionSchema[];
+}) {
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 4,
+      }}
+    >
+      {headerTemplate.map((template) => (
+        <View
+          key={template.id}
+          style={{
+            width: template.style.width,
+            height: "100px",
+            border: "1px solid black",
+          }}
+        >
+          {template.contentType === "text" && template.content !== null && (
+            <RenderText content={template.content as JSONContent} />
+          )}
+
+          {template.contentType === "img" && template.content !== null && (
+            <RenderImage content={template.content as TImageContent} />
+          )}
+        </View>
+      ))}
+    </View>
+  );
+}
+
+function Footer({
+  footerTemplate,
+}: {
+  footerTemplate: TTemplateSectionSchema[];
+}) {
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 4,
+      }}
+    >
+      {footerTemplate.map((template) => (
+        <View
+          key={template.id}
+          style={{
+            width: template.style.width,
+            height: "100px",
+            border: "1px solid black",
+          }}
+        >
+          {template.contentType === "text" && template.content !== null && (
+            <RenderText content={template.content as JSONContent} />
+          )}
+
+          {template.contentType === "img" && template.content !== null && (
+            <RenderImage content={template.content as TImageContent} />
+          )}
+        </View>
+      ))}
+    </View>
+  );
+}
+
+function RenderText({ content }: { content: JSONContent }) {
+  return (
+    <View>
+      <Text>{JSON.stringify(content)}</Text>
+    </View>
+  );
+}
+
+function RenderImage({ content }: { content: TImageContent }) {
+  if (content === null) {
+    return (
+      <View
+        style={{
+          width: "100px",
+          height: "100px",
+          overflow: "hidden",
+          marginHorizontal: "auto",
+          borderRadius: "1000",
+        }}
+      >
+        <Image
+          src={"https://placehold.co/100x100"}
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View
+      style={{
+        width: "100px",
+        height: "100px",
+        overflow: "hidden",
+        marginHorizontal: "auto",
+        borderRadius: "1000",
+      }}
+    >
+      <Image
+        src={content.buffer}
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
+    </View>
   );
 }
