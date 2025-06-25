b85f7959df3ae8f1bd9a30b0d53f9dc1be668b12
Rifat Khan <rifat6633@gmail.com>
2025-06-25 22:27:21 +0600

    chore(web): add new nanoId.

--- apps/web/src/lib/nanoid/index.ts
+++ apps/web/src/lib/nanoid/index.ts
@@ -1,6 +1,7 @@
 import { customAlphabet } from "nanoid";
 
 export const nanoId = {
+  id: () => customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 5)(),
   medId: () => customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 5)(),
   sectionId: () => customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 5)(),
   templateId: () => customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 5)(),
